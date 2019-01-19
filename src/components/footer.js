c188a0a2f93538716840920afe2feaa25bdb62cd
Davide Porrovecchio <pdavide@gmail.com>
2019-01-19 11:29:37 +0100

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,16 +7,26 @@ import {
   Row
 } from 'reactstrap';
 import Icon from './icon';
+import AgidLogo from '../images/logo-agid.svg';
+import DTLogo from '../images/logo-team-digitale.svg';
 
 const Footer = props => (
   <footer className="neutral-1-bg-a9">
     <Container>
       <Row className="py-4">
         <Col xs="12" sm="6" md="4">
-
+          <div className="px-2 py-3">
+            <a href="https://www.agid.gov.it/" target="_blank">
+              <img src={AgidLogo} alt="Agenzia per l'Italia Digitale" height="36" style={{ maxWidth: `100%` }} />
+            </a>
+          </div>
         </Col>
-        <Col xs="12" sm="6" md="4">
-
+        <Col xs="12" sm="6" md="4" className="d-flex">
+          <div className="px-2 py-2">
+            <a href="https://www.agid.gov.it/" target="_blank">
+              <img src={DTLogo} alt="Agenzia per l'Italia Digitale" height="48" style={{ maxWidth: `100%` }} />
+            </a>
+          </div>
         </Col>
         <Col xs="12" sm="6" md="4" className="text-white text-right pt-2">
           <span>Seguici su</span>
@@ -29,7 +39,7 @@ const Footer = props => (
                   href={socialLink.url}
                   aria-label={socialLink.name}
                 >
-                  <Icon icon={socialLink.icon} className="icon-sm icon-light align-top"/>
+                  <Icon icon={socialLink.icon} className="icon-sm icon-light align-top" />
                 </a>
               );
             })
