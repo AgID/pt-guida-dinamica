ade2f2d7d73c91c0c750a5970a184e1dd4eeb4cd
Davide Porrovecchio <pdavide@gmail.com>
2019-01-22 17:13:34 +0100

    Generate pages dinamically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,55 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
+const fs = require('fs');
+const yaml = require('js-yaml');
 
-// You can delete this file if you're not using it
+exports.createPages = ({ actions }) => {
+  const { createPage } = actions;
+
+  try {
+    const pages = yaml.safeLoad(fs.readFileSync(path.resolve(`src/data/pages.yml`), 'utf8'));
+    Object.keys(pages).map((page, pageIndex, pagesArray) => {
+      createMarkdownPages(createPage, pages[pageIndex], getPageNav(
+        pages[pageIndex].slug,
+        null,
+        (pageIndex - 1) in pagesArray ? pages[pageIndex - 1].slug : null,
+        (pageIndex + 1) in pagesArray ? pages[pageIndex + 1].slug : null,
+        pages[pageIndex].subtree ? pages[pageIndex].subtree[0].slug : null
+      ));
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createMarkdownPages = (createPage, page, pageNav) => {
+  const pageTemplate = path.resolve(`src/templates/page.js`);
+  const pagePath = [(pageNav.parentPath || '').replace(/\/$/, ''), page.slug].join('/');
+
+  createPage({
+    path: pagePath,
+    component: pageTemplate,
+    context: {
+      filenameRegex: `/${page.name}.md$/`,
+      pageNav: JSON.stringify(pageNav)
+    }
+  });
+
+  page.subtree && page.subtree.map((subPage, subPageIndex) => {
+    createMarkdownPages(createPage, subPage, getPageNav(
+      [pagePath.replace(/\/$/, ''), subPage.slug].join('/'),
+      pagePath,
+      (subPageIndex - 1) in page.subtree ? page.subtree[subPageIndex - 1].slug : null,
+      (subPageIndex + 1) in page.subtree ? page.subtree[subPageIndex + 1].slug : null,
+      subPage.subtree ? subPage.subtree[0].slug : null
+    ));
+  });
+};
+
+const getPageNav = (current, parent, prev, next, firstChild) => {
+  return {
+    parentPath: parent,
+    prevPath: (prev !== null) && [(parent || '').replace(/\/$/, ''), prev].join('/'),
+    nextPath: (next !== null) && [(parent || '').replace(/\/$/, ''), next].join('/'),
+    firstChildPath: firstChild && [current, firstChild].join('/')
+  };
+};
